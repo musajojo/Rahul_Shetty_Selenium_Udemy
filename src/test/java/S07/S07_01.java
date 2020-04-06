c1f528cae340b3fc69cc8b45f67bd3a1c62ba460
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-04-06 17:23:04 -0400

    Code Updates

--- src/test/java/S07/S07_01.java
+++ src/test/java/S07/S07_01.java
@@ -1,7 +1,26 @@
 package S07;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class S07_01 {
     public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        String googlrURL = "http://google.com";
+        driver.get(googlrURL);
+
+        // Xpath
+        // Parent Child Relationship: this is used when there is no unique attributes to identify the object.
+        // Go from Parent to Child using tags. Use the unique tag of the Parent then traverse to the Child
+
+
+
+
+
+
+
 
     }
 }
