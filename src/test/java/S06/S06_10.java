d132c08656c074281dcc3c048df1ff292fee535f
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-04-14 17:17:27 -0400

    Code Updates

--- src/test/java/S06/S06_10.java
+++ src/test/java/S06/S06_10.java
@@ -17,11 +17,41 @@ public class S06_10 {
 
         // Customized CSS
 
+        // tagName[attribute='value'] --> CSS Syntax
+        // input[name='email'] --> actual CSS
 
+        // If you want to to skip the tag name you can as well
+        // [name='value']
 
-        driver.findElement(By.cssSelector("")).sendKeys("Hello");
-        driver.findElement(By.cssSelector("")).sendKeys("123456");
-        driver.findElement(By.cssSelector("")).click();
+        // Another Syntax is:
+        // tagName#id
+        // input#u_0_b
+
+        // Additionl Syntax
+        // tagName.className
+        // h2._8est
+
+        // Additional Syntax
+        // #id
+        // #u_0_b
+
+        // to check CSS in Console put it in $("") --> $("input[name='email']")
+
+        // Email Box
+        // <input type="email" class="inputtext login_form_input_box" name="email" id="email" data-testid="royal_email">
+
+        // Password Box
+        // <input type="password" class="inputtext login_form_input_box" name="pass" id="pass" data-testid="royal_pass">
+
+        // Login Button
+        //<input value="Log In" aria-label="Log In" data-testid="royal_login_button" type="submit" id="u_0_b">
+
+        System.out.println(driver.findElement(By.cssSelector("h2._8est")).getText()); // tagName.className --> 'Sign Up' Text
+        System.out.println(driver.findElement(By.cssSelector("#u_0_13")).getText()); // #id --> 'Sign Up' Button
+
+        driver.findElement(By.cssSelector("input[name='email']")).sendKeys("Hello"); // tagName[attribute='value']
+        driver.findElement(By.cssSelector("[name='pass']")).sendKeys("123456"); // [name='value']
+        driver.findElement(By.cssSelector("input#u_0_b")).click(); // input#u_0_b
 
         Thread.sleep(2000);
         driver.quit();
