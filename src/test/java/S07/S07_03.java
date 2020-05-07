e2692b1dbdd6b18319f8eb4b8fd0c6ba8cdbb81a
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-06 20:08:14 -0400

    Code Updates

--- src/test/java/S07/S07_03.java
+++ src/test/java/S07/S07_03.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class S07_03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -15,15 +15,31 @@ public class S07_03 {
         /* ------------------------------------------------------
        // How to identify siblings and traverse back to parent nodes with xpath?
 
+       Identifying sibling by using siblings. This is rare, for example
+       if the the parent is dynamic and the child is static then find the static child
+       and then you can go from there and find its sibling. Use->  following-sibling::
+       example: "//*[@id=\'content\']/ul/li[1]/following-sibling::li[2]"
 
-
+       We can also traverse from child to parent in Xpath by using the following->  parent::ul
+       example: "//*[@id='content']/ul/li[5]/parent::ul"
 
          ------------------------------------------------------*/
-
+        // A/B Testing
         System.out.println(driver.findElement(By.xpath("//*[@id=\'content\']/ul/li[1]")).getText());
-        System.out.println(driver.findElement(By.xpath("//*[@id=\'content\']/ul/li[1]/following-sibling::li[2]")).getText());
-        driver.findElement(By.xpath("//*[@id=\'content\']/ul/li[1]/following-sibling::li[2]")).click();
-        driver.quit();
+        // Add/Remove Elements
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("//*[@id=\'content\']/ul/li[1]/following-sibling::li[1]")).getText());
+        // Add/Remove Elements - Regular Xpath
+        driver.findElement(By.xpath("//*[@id=\'content\']/ul/li[2]/a")).click();
 
+        Thread.sleep(2000);
+        driver.navigate().back();
+
+        //Broken Images
+        System.out.println(driver.findElement(By.xpath("//*[@id='content']/ul/li[5]")).getText());
+       // Parent - This is the whole list of ul's
+        System.out.println(driver.findElement(By.xpath("//*[@id='content']/ul/li[5]/parent::ul")).getText());
+
+        driver.quit();
     }
 }
