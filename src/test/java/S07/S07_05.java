e01cf132059a73d2a7ce1540f12c6c6e48a1b605
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-08 01:06:07 -0400

    Code Updates

--- src/test/java/S07/S07_05.java
+++ src/test/java/S07/S07_05.java
@@ -10,20 +10,38 @@ public class S07_05 {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         String googlrURL = "http://google.com";
-        driver.get("http://practice.cybertekschool.com/");
+        String cybertekPracticeURL = "http://practice.cybertekschool.com/";
+        String saleforceLoginURL = "https://login.salesforce.com/";
+        driver.get(saleforceLoginURL);
         driver.manage().window().maximize();
         /*
         ------------------------------------------------------
        //46. Css Selectors locators
 
+       xpath = //tagname[@attribute='value'] --> //input[@id='username'] -->
+       //*[@id='username'] -> Only put star if you are sure it is unique, in this case tagname is not necessary
 
+       Customizable Css:
+       Css = tagname[attribute='value'] --> input[id='username'] -->
+       [id='username'] -> also a short way ti write Css
+
+       #id --> #Login
+
+       If there is a class name and you want to shorten it add a dot (.) at the beginning,
+       also if there are any spaces in the classname replace the space with a dot (.).
+       .classname --> .fl.pr.db.tn3
          ------------------------------------------------------
          */
 
-        System.out.println(driver.findElement(By.cssSelector("")).getText());
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("")).click();
-        Thread.sleep(2000);
+        //UserName
+        driver.findElement(By.cssSelector("input[name='username']")).sendKeys("Hello");
+        //Password
+        driver.findElement(By.cssSelector("[id='password']")).sendKeys("123456");
+        //Login Button
+        driver.findElement(By.cssSelector("#Login")).click();
+        Thread.sleep(1000);
+        //Remember me button
+        System.out.println(driver.findElement(By.cssSelector(".fl.pr.db.tn3")).getText());
 
         driver.quit();
     }
