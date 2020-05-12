3636fa4810363abb57aaa05156fa335f529ced96
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-12 02:21:45 -0400

    Handling Checkbox and getting the size of them with Selenium

---
+++ src/test/java/S08/S08_06.java
@@ -0,0 +1,32 @@
+package S08;
+// Section 8, Video 56,
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class S08_06 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        String googlrURL = "http://google.com";
+        String cybertekPracticeURL = "http://practice.cybertekschool.com/";
+        String rsAcadamyDdPrcticeURL = "https://rahulshettyacademy.com/dropdownsPractise/";
+        String spiceJetURL = "https://www.spicejet.com/";
+        String makeMyTripURL = "https://www.makemytrip.com/?ccde=us";
+        driver.get(makeMyTripURL);
+        driver.manage().window().maximize();
+        /*//------------------------------------------------------
+        56. Handling Checkbox and getting the size of them with Selenium
+
+
+        //------------------------------------------------------*/
+
+        driver.findElement(By.id(""));
+
+        driver.quit();
+    }
+}
