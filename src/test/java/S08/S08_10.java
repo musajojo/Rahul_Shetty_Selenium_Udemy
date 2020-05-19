bbf8d599779c800a18b97777253a263254356c2c
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-19 03:30:32 -0400

    Commit

--- src/test/java/S08/S08_10.java
+++ src/test/java/S08/S08_10.java
@@ -1,12 +1,11 @@
 package S08;
-// Section 8, Video 59,
+// Section 8, Video 60,
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 
-public class S08_09 {
+public class S08_10 {
     public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
@@ -19,17 +18,12 @@ public class S08_09 {
         driver.get(spiceJetURL);
         driver.manage().window().maximize();
         /*//------------------------------------------------------
-        59. Handling Calendar UI in travel websites using Selenium
+        60. Validating if UI Elements are disabled or enabled with Attributes
 
         //------------------------------------------------------*/
-        Thread.sleep(3000);
-        //Clicks on Calender for us to see the dates.
-        driver.findElement(By.className("ui-datepicker-trigger")).click();
-        Thread.sleep(1000);
-        //Clicks whatever the current date is.
-        // In this situation the className is tied to whatever the current date is,
-        // so the date it picks will change on a daily basis, but the className will remain the same.
-        driver.findElement(By.className("ui-datepicker-today")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.className("")).click();
 
         driver.quit();
     }
