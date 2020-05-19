beab03d800b98f7a6c73cfb0ec6e837f800a6d72
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-19 18:39:55 -0400

    Assignment 2: UI(Dropdowns,EditBoxes,Error Validation…

--- src/test/java/S08/S08_14_Assignment_02.java
+++ src/test/java/S08/S08_14_Assignment_02.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class S08_13_Assignment_02 {
+public class S08_14_Assignment_02 {
     public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
@@ -41,7 +41,7 @@ public class S08_13_Assignment_02 {
         // More Options link
         driver.findElement(By.id("MoreOptionsLink")).click();
         // Preferred Airline box
-        driver.findElement(By.id("AirlineAutocomplete")).click();
+        driver.findElement(By.id("AirlineAutocomplete")).sendKeys("Super Jet");
         // Search button
         driver.findElement(By.id("SearchBtn")).click();
 
@@ -51,5 +51,53 @@ public class S08_13_Assignment_02 {
         driver.quit();
     }
 }
+
+/*
+Instructor Example:
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assignment {
+public static void main(String[] args) {
+System.setProperty("webdriver.chrome.driver", "C://work//chromedriver.exe");
+WebDriver driver=new ChromeDriver();
+driver.get("http://www.cleartrip.com/");
+
+//calendar
+driver.findElement(By.id("DepartDate")).click();
+driver.findElement(By.cssSelector("a.ui-state-default.ui-state-highlight.ui-state-active ")).click();
+
+//DD1
+WebElement adult=driver.findElement(By.id("Adults"));
+Select s =new Select(adult);
+s.selectByIndex(2);
+
+//DD2
+WebElement ch=driver.findElement(By.id("Childrens"));
+Select s1 =new Select(ch);
+s1.selectByIndex(2);
+
+driver.findElement(By.xpath("//a[@title='More search options']")).click();
+
+driver.findElement(By.id("AirlineAutocomplete")).sendKeys("indigo");
+
+
+
+driver.findElement(By.id("SearchBtn")).click();
+
+//validate error message
+
+System.out.println(driver.findElement(By.id("homeErrorMessage")).getText());
+
+    }
+
+}
+
+ */
 
 
